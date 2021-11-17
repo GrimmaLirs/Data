6fb3d170f5732d5e64e08288ec572abb9454d386
Sergey <ksenof.serrr@mail.ru>
2021-11-17 10:05:19 +0300

    Создан конструктор, перегружен оператор вывода.

--- Data/Data.h
+++ Data/Data.h
@@ -1,25 +1,78 @@
 #include <iostream>
 
-
 class Data
 {
-private: int day, month, year;
+	private: unsigned int day, month, year;
+	private: bool leap_year;
 
-public: Data()
-{
-		day = 0; 
-		month = 0; 
-		year = 0;
-}
-public: Data(int day_, int month_, int year_)
-{
-	if (((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0))//Проверка на високосный год
+	public: Data()
 	{
-		//leap year
+			day = 0; 
+			month = 0; 
+			year = 0;
+			leap_year = false;
 	}
-	day = day_;
-	month = month_;
-	year = year_;
+	public: Data(unsigned int day_, unsigned int month_, unsigned int year_)
+	{	
+		day = day_;
+		month = month_;
+		year = year_;
+
+		year += MonthToYears(month);
+		DaysInMonth(day, month);
+		year += MonthToYears(month);
+		if (((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0)))//Проверка на високосный год
+		{
+			leap_year = true;
+		}
+		DaysInMonth(day, month);
+	}
+		  friend std::ostream& operator<< (std::ostream& out, const Data& data);
+
+private: unsigned int MonthToYears(unsigned int& month)
+{
+	unsigned int years = 0;
+	if (month > 12)
+	{
+		for (; month > 12; month - 12)
+			years++;
+	}
+	return years;
+}
+private: void DaysInMonth(unsigned int& day, unsigned int& month)
+{
+	unsigned int maxDaysInMonth;
+	if (month == 2)
+	{
+		if (leap_year)
+			maxDaysInMonth = 29;
+		else
+			maxDaysInMonth = 28;
+	}
+	if (month % 2 == 0 && month != 2)
+	{
+		maxDaysInMonth = 30;
+
+	}
+	else
+	{
+		maxDaysInMonth = 31;
+	}
+	for (; day > maxDaysInMonth;)
+	{
+		day - maxDaysInMonth;
+		month++;
+	}
 }
 };
 
+
+std::ostream& operator<< (std::ostream& out, const Data& data)
+{
+	// Поскольку operator << является другом класса Point, то мы имеем прямой доступ к членам Point
+	if (data.month < 10)
+		out << data.day << ".0" << data.month << "." << data.year;
+	else
+		out << data.day << "." << data.month << "." << data.year;
+	return out;
+}
