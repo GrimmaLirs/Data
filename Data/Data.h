bd2b752cbcc47648d666b27a4033c02ba79b80b4
Sergey <ksenof.serrr@mail.ru>
2021-11-21 16:48:52 +0300

    Была добавлена перегрузка операторов по умолчанию

--- Data/Data.h
+++ Data/Data.h
@@ -7,9 +7,9 @@ class Data
 
 	public: Data()
 	{
-			day = 0; 
-			month = 0; 
-			year = 0;
+			day = 1; 
+			month = 1; 
+			year = 1;
 			leap_year = false;
 	}
 	public: Data(unsigned int day_, unsigned int month_, unsigned int year_)
@@ -17,70 +17,208 @@ class Data
 		day = day_;
 		month = month_;
 		year = year_;
+		TideUp();
+	}
+		  friend std::ostream& operator<< (std::ostream& out, const Data& data);
+		  friend std::istream& operator>> (std::istream& in, Data& data);
 
+	private: void TideUp() //Метод для форматирования даты
+	{
 		year += MonthToYears(month);
-		DaysInMonth(day, month);
+		DaysToMonth(day, month);
 		year += MonthToYears(month);
 		if (((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0)))//Проверка на високосный год
 		{
 			leap_year = true;
 		}
-		DaysInMonth(day, month);
+		DaysToMonth(day, month);
 	}
-		  friend std::ostream& operator<< (std::ostream& out, const Data& data);
 
-private: unsigned int MonthToYears(unsigned int& month)
-{
-	unsigned int years = 0;
-	if (month > 12)
+	private: unsigned int MonthToYears(unsigned int& month)
 	{
-		for (; month > 12; month -= 12)
-			years++;
+		unsigned int years = 0;
+		if (month > 12)
+		{
+			for (; month > 12; month -= 12)
+				years++;
+		}
+		return years;
 	}
-	return years;
-}
-private: void DaysInMonth(unsigned int& day, unsigned int& month)
-{
-	unsigned int maxDaysInMonth;
-	switch (month)
+
+	private: int DaysInMonth()
 	{
-	case 1:maxDaysInMonth = 31; break;
-	case 2:
-		if (leap_year == true)
-			maxDaysInMonth = 29;
+		unsigned int maxDaysInMonth = 0;
+		switch (month)
+		{
+		case 1:maxDaysInMonth = 31; break;
+		case 2:
+			if (leap_year == true)
+				maxDaysInMonth = 29;
+			else
+				maxDaysInMonth = 28;
+			break;
+		case 3:maxDaysInMonth = 31; break;
+		case 4:maxDaysInMonth = 30; break;
+		case 5:maxDaysInMonth = 31; break;
+		case 6:maxDaysInMonth = 30; break;
+		case 7:maxDaysInMonth = 31; break;
+		case 8:maxDaysInMonth = 31; break;
+		case 9:maxDaysInMonth = 30; break;
+		case 10:maxDaysInMonth = 31; break;
+		case 11:maxDaysInMonth = 30; break;
+		case 12:maxDaysInMonth = 31; break;
+
+		default:
+			maxDaysInMonth = 31;break;	
+		}
+		return maxDaysInMonth;
+	}
+
+	private: void DaysToMonth(unsigned int& day, unsigned int& month)
+	{
+		for (; day > DaysInMonth();)
+		{
+			day -= DaysInMonth();
+			month++;
+		}
+	}
+
+	public: int GetDay()
+	{
+	   return day;
+	}
+
+	public: void SetDay(int newDay)
+	{
+		day = newDay;
+		TideUp();
+	}
+	public: int GetMonth()
+	{
+		return month;
+	}
+
+	public: void SetMonth(int newMonth)
+	{
+		month = newMonth;
+		TideUp();
+	}
+
+	public: int GetYear()
+	{
+		return this->year;
+	}
+
+	public: void SetYear(int newYear)
+	{
+		year = newYear;
+	}
+	Data operator = (Data data)
+	{
+		this->SetDay(data.GetDay());
+		this->SetMonth(data.GetMonth());
+		this->SetYear(data.GetYear());
+		return data;
+	}
+	// префиксные операторы
+	Data& operator++ ()
+	{
+		this->SetDay(this->GetDay() + 1);
+		return *this;
+	}
+	Data& operator-- ()
+	{
+		
+		if (this->GetDay() - 1 <= 0)
+		{
+			
+			if (this->GetMonth() - 1 <= 0)
+			{
+				this->SetMonth(12);
+				this->SetYear(this->GetYear() - 1);
+			}
+			else
+				this->SetMonth(this->GetMonth() - 1);
+			this->SetDay(DaysInMonth());
+		}
 		else
-			maxDaysInMonth = 28;
-		break;
-	case 3:maxDaysInMonth = 31; break;
-	case 4:maxDaysInMonth = 30; break;
-	case 5:maxDaysInMonth = 31; break;
-	case 6:maxDaysInMonth = 30; break;
-	case 7:maxDaysInMonth = 31; break;
-	case 8:maxDaysInMonth = 31; break;
-	case 9:maxDaysInMonth = 30; break;
-	case 10:maxDaysInMonth = 31; break;
-	case 11:maxDaysInMonth = 30; break;
-	case 12:maxDaysInMonth = 31; break;
-
-	default:
-		break;
+			this->SetDay(this->GetDay() - 1);
+		return *this;
 	}
-	
-	for (; day > maxDaysInMonth;)
+	// постфиксные операторы
+	Data operator++ (int)
 	{
-		day -= maxDaysInMonth;
-		month++;
+		Data prev = *this;
+		++* this;
+		return prev;
 	}
-}
+	Data operator-- (int)
+	{
+		Data prev = *this;
+		--* this;
+		return prev;
+	}
 };
 
 
 std::ostream& operator<< (std::ostream& out, const Data& data)
 {
-	// Поскольку operator << является другом класса Point, то мы имеем прямой доступ к членам Point
+	// Поскольку operator << является другом класса Data, то мы имеем прямой доступ к членам Data
 	if (data.month < 10)
 		out << data.day << ".0" << data.month << "." << data.year;
 	else
 		out << data.day << "." << data.month << "." << data.year;
 	return out;
+}
+
+std::istream& operator>> (std::istream& in, Data& data)
+{
+	// Поскольку operator>> является другом класса Data, то мы имеем прямой доступ к членам Data.
+	// Обратите внимание, параметр Data (объект класса Data) должен быть неконстантным, чтобы мы имели возможность изменить члены класса
+	in >> data.day;
+	in >> data.month;
+	in >> data.year;
+	return in;
+}
+
+Data operator + (Data data, unsigned int days)
+{
+	data.SetDay(days + data.GetDay());
+	return data;
+}
+
+Data operator += (Data& data, unsigned int days)
+{
+	data.SetDay(days + data.GetDay());
+	return data;
+}
+
+bool operator == (Data data1, Data data2)
+{
+	return (data1.GetDay() == data2.GetDay() && data1.GetMonth() == data2.GetMonth() && data1.GetYear() == data2.GetYear());
+}
+bool operator != (Data data1, Data data2)
+{
+	return (data1.GetDay() != data2.GetDay() || data1.GetMonth() != data2.GetMonth() || data1.GetYear() != data2.GetYear());
+}
+
+
+bool operator > (Data data1, Data data2)
+{
+	return (data1.GetYear() > data2.GetYear() || (data1.GetYear() == data2.GetYear() && data1.GetMonth() > data2.GetMonth()) || (data1.GetYear() == data2.GetYear() && data1.GetMonth() == data2.GetMonth() && data1.GetYear() > data2.GetYear()));
+}
+
+bool operator < (Data data1, Data data2)
+{
+	return (data1.GetYear() < data2.GetYear() || (data1.GetYear() == data2.GetYear() && data1.GetMonth() < data2.GetMonth()) || (data1.GetYear() == data2.GetYear() && data1.GetMonth() == data2.GetMonth() && data1.GetYear() < data2.GetYear()));
+}
+
+bool operator >= (Data data1, Data data2)
+{
+	return (data1.GetYear() >= data2.GetYear() || (data1.GetYear() == data2.GetYear() && data1.GetMonth() >= data2.GetMonth()) || (data1.GetYear() == data2.GetYear() && data1.GetMonth() == data2.GetMonth() && data1.GetYear() >= data2.GetYear()));
+}
+
+bool operator <= (Data data1, Data data2)
+{
+	return (data1.GetYear() <= data2.GetYear() || (data1.GetYear() == data2.GetYear() && data1.GetMonth() <= data2.GetMonth()) || (data1.GetYear() == data2.GetYear() && data1.GetMonth() == data2.GetMonth() && data1.GetYear() <= data2.GetYear()));
 }
--- Data/Main.cpp
+++ Data/Main.cpp
@@ -7,11 +7,61 @@ int main()
 	Data dataMuch(40, 15, 2020);
 	Data dataLeap(60, 1, 2024);
 	Data dataNotLeap(60, 1, 2025);
+	Data dataSmall (1, 1, 2000);
+	Data dataMidle(31, 12, 2010);
+	Data dataLarge(31, 12, 2100);
 	cout << data << "\n";
 	cout << dataNew << "\n";
 	cout << dataMuch << "\n";
 	cout << dataLeap << "\n";
 	cout << dataNotLeap << "\n";
+	cout << data << "\n";
+	//cin >> data;
+	//data = data + 365;
+	data = dataNew;
+	cout << data << "\n";
+//Тестируем операции == и !=
+	if (data == dataNew)
+		cout << "Work";
+	else 
+		cout << "Dont Work";
+	cout << "\n";
+	if (data != dataMuch)
+		cout << "Work";
+	else
+		cout << "Dont Work";
+	cout << "\n";
+//Тестируем операции > и <
+	if (dataMidle > dataSmall)
+		cout << "Work";
+	else
+		cout << "Dont Work";
+	cout << "\n";
+	if (dataMidle < dataLarge)
+		cout << "Work";
+	else
+		cout << "Dont Work";
+	cout << "\n";
+//Тестируем операции >= и <=
+	if (dataMidle >= dataSmall && dataMidle >= dataMidle)
+		cout << "Work";
+	else
+		cout << "Dont Work";
+	cout << "\n";
+	if (dataMidle <= dataLarge && dataMidle <= dataMidle)
+		cout << "Work";
+	else
+		cout << "Dont Work";
+	cout << "\n";
+//Тестируем операции ++ и --
+	Data dataLow;
+	cout << dataLow << "\n";
+	cout << dataLow-- << " Date befor -- \n";
+	cout << dataLow++ << " Date befor ++ \n";
+	cout << dataLow << "\n";
+	cout << --dataLow << " Date after -- \n";
+	cout << ++dataLow << " Date after ++ \n";
+	cout << dataLow << "\n";
 	system("pause");
 	return 0;
 }
